2ac6d95f0452da2cf519c3736ad64248b5175158
Sai Chen <saichenlaioffer@gmail.com>
2022-02-09 23:54:14 -0800

    add KthSmallestElementinaSortedMatrix.java

---
+++ KthSmallestElementinaSortedMatrix.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+
+public class KthSmallestElementinaSortedMatrix {
+    // pq - time: O(m * n) space: O(m * n)
+    public int kthSmallest(int[][] matrix, int k) {
+        Queue<int[]> q = new PriorityQueue<>((a, b) -> {
+            return matrix[a[0]][a[1]] - matrix[b[0]][b[1]];
+        });
+        int m = matrix.length, n = matrix[0].length;
+        Set<Integer> set = new HashSet<>();
+        q.offer(new int[]{0, 0});
+        while (q.size() > 0) {
+            int[] cur = q.poll();
+            k--;
+            if (k == 0) {
+                return matrix[cur[0]][cur[1]];
+            }
+            if (cur[0] + 1 < m && set.add((cur[0] + 1) * n + cur[1])) {
+                q.offer(new int[]{cur[0] + 1, cur[1]});
+            }
+            if (cur[1] + 1 < n && set.add(cur[0] * n + cur[1] + 1)) {
+                q.offer(new int[]{cur[0], cur[1] + 1});
+            }
+        }
+        return -1;
+    }
+}
