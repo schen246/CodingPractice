b10e220b4c1a22716d914f5061171c4a05eefaca
Sai Chen <saichenlaioffer@gmail.com>
2022-02-09 18:03:38 -0800

    add BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            int m = (i + j) / 2;
+            if (nums[m] == target) {
+                return m;
+            }
+            if (nums[m] < target) {
+                i = m + 1;
+            } else {
+                j = m - 1;
+            }
+        }
+        return -1;
+    }
+}
+
+// Time: O(logn)
+// Space: O(1)
