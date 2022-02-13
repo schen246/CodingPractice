319cfe7cd9f1d809bbedca3a70477d9ddd088182
Sai Chen <saichenlaioffer@gmail.com>
2022-02-12 19:13:41 -0800

    add ReverseLinkedList.java

---
+++ ReverseLinkedList.java
@@ -0,0 +1,24 @@
+public class ReverseLinkedList {
+    // recursion - time: O(n) space: O(n)
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    // iteration - time: O(n) space: O(1)
+    public ListNode reverseList2(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
