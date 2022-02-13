894a36bafa581c286b24eb39f82af83343b9ea74
Sai Chen <saichenlaioffer@gmail.com>
2022-02-12 22:40:31 -0800

    add Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        AddStrings obj = new AddStrings();
+        String s1 = "11";
+        String s2 = "123";
+        String res = obj.addStrings(s1, s2);
+        System.out.println(res);
+    }
+}
