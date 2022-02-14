81e436a1e609d31cf66cad55329ffd7ffba78df7
Sai Chen <saichenlaioffer@gmail.com>
2022-02-13 23:40:46 -0800

    add LowestCommonAncestorofaBinaryTree.java

---
+++ LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,14 @@
+public class LowestCommonAncestorofaBinaryTree {
+    // recursion - time: O(n) space: O(height)
+    public TreeNode lowestCommonAncestor(TreeNode root, int p, int q) {
+        if (root == null || root.val == p || root.val == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left == null ? right : left;
+    }
+}
