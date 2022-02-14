dde3cd8fc0a96ef5137b36bcb7e028de576d0dfb
Sai Chen <saichenlaioffer@gmail.com>
2022-02-13 22:34:22 -0800

    update FloodFill.java

--- FloodFill.java
+++ FloodFill.java
@@ -1,3 +1,6 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class FloodFill {
     // dfs + inplace dedup - time: O(m * n) space: O(m * n)
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
@@ -15,5 +18,26 @@ public class FloodFill {
         for (int[] dir : dirs) {
             dfs(image, r + dir[0], c + dir[1], color, newColor);
         }
+    }
+
+    // bfs - time: O(m * n) space: O(m * n)
+    public int[][] floodFill2(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{sr, sc});
+        image[sr][sc] = newColor;
+        int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int[] dir : dirs) {
+                int x = cur[0] + dir[0];
+                int y = cur[1] + dir[1];
+                if (x >= 0 && x < image.length && y >= 0 && y < image[0].length && image[x][y] == color) {
+                    q.offer(new int[]{x, y});
+                    image[x][y] = newColor;
+                }
+            }
+        }
+        return image;
     }
 }
