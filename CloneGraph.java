22e1eb31465d96d5ca0737e66a8d1914643fa411
Sai Chen <saichenlaioffer@gmail.com>
2022-02-15 00:47:11 -0800

    update CloneGraph.java

--- CloneGraph.java
+++ CloneGraph.java
@@ -1,5 +1,7 @@
+import java.util.ArrayDeque;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Queue;
 
 public class CloneGraph {
     public Node cloneGraph(Node node) {
@@ -20,5 +22,27 @@ public class CloneGraph {
             newNode.neighbors.add(dfs(nei, map));
         }
         return newNode;
+    }
+
+    // bfs - time: O(n) space: O(n)
+    public Node cloneGraph2(Node node) {
+        if (node == null) {
+            return null;
+        }
+        Map<Node, Node> map = new HashMap<>();
+        Queue<Node> q = new ArrayDeque<>();
+        map.put(node, new Node(node.val));
+        q.offer(node);
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+            for (Node nei : cur.neighbors) {
+                if (!map.containsKey(nei)) {
+                    map.put(nei, new Node(nei.val));
+                    q.offer(nei);
+                }
+                map.get(cur).neighbors.add(map.get(nei));
+            }
+        }
+        return map.get(node);
     }
 }
