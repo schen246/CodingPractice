f1224a78bcfe7eed56946cff740c769e3e47212f
Sai Chen <saichenlaioffer@gmail.com>
2022-02-14 18:35:53 -0800

    add InsertintoaBinarySearchTree.java

---
+++ InsertintoaBinarySearchTree.java
@@ -0,0 +1,14 @@
+public class InsertintoaBinarySearchTree {
+    // binary search - time: O(logn) space: O(height)
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+}
