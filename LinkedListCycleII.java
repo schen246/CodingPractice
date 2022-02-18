2fb5d682bb30bce942c3153ee1985f02da2a3854
Sai Chen <saichenlaioffer@gmail.com>
2022-02-17 21:20:52 -0800

    add LinkedListCycleII.java

---
+++ LinkedListCycleII.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (!set.add(head)) {
+                return head;
+            }
+            head = head.next;
+        }
+        return null;
+    }
+}
