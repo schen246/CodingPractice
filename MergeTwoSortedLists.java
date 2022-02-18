d7e41f73c78c69bfbe130534220e2b03d1b2e2c0
Sai Chen <saichenlaioffer@gmail.com>
2022-02-17 19:44:40 -0800

    add MergeTwoSortedLists.java

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,27 @@
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode d = new ListNode(0);
+        ListNode cur = d;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        while (l1 != null) {
+            cur.next = l1;
+            l1 = l1.next;
+            cur = cur.next;
+        }
+        while (l2 != null) {
+            cur.next = l2;
+            l2 = l2.next;
+            cur = cur.next;
+        }
+        return d.next;
+    }
+}
