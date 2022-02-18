0c695232f9924d20698f890add07123fde1ade26
Sai Chen <saichenlaioffer@gmail.com>
2022-02-17 23:22:11 -0800

    add TwoSumIIIDatastructuredesign.java

---
+++ TwoSumIIIDatastructuredesign.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumIIIDatastructuredesign {
+    Map<Integer, Integer> map;
+    
+    /** Initialize your data structure here. */
+    public TwoSumIIIDatastructuredesign() {
+        map = new HashMap<>();
+    }
+    
+    /** Add the number to an internal data structure.. */
+    public void add(int number) {
+        map.put(number, map.getOrDefault(number, 0) + 1);
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+        for (int num : map.keySet()) {
+            if (num != value - num && map.containsKey(value - num)) {
+                return true;
+            }
+            if (num == value - num && map.get(num) >= 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
