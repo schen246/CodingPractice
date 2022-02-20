c384a7439e53864501237487b1d2f7da73f86aff
Sai Chen <saichenlaioffer@gmail.com>
2022-02-19 23:27:22 -0800

    add ValidPalindrome.java

---
+++ ValidPalindrome.java
@@ -0,0 +1,18 @@
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+            if (i >= j) {
+                return true;
+            }
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
